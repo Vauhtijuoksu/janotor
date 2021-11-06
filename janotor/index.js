fb55324f3150ebf5b4a303a755907179fc947520
Aleksi Seppänen <aleksi@seppanen.dev>
2021-11-06 18:00:55 +0200

    Add correct async handler

--- janotor/index.js
+++ janotor/index.js
@@ -4,6 +4,7 @@ module.exports = async function (context) {
     const apiResp = await axios.get(process.env.wwfUrl)
     const vauhtiApiResp = await axios.get(process.env.vauhtisUrl)
 
+    const promises = [];
     const keys = Object.keys(apiResp.data.entities)
     for await (const id of keys) {
         if (vauhtiApiResp.data.some(e => e.external_id === id)) {
@@ -21,12 +22,17 @@ module.exports = async function (context) {
             external_id: id
         }
 
-        axios.post(process.env.vauhtisUrl, donation, {
+        const result = axios.post(process.env.vauhtisUrl, donation, {
             auth: {
                 username: process.env.vauhtisUsername,
                 password: process.env.vauhtisPassword
             }
+        }).catch(err => {
+            context.log(err);
         });
+        promises.push(result);
         context.log(`Added donation with id ${id}`)
     }
+
+    await Promise.all(promises);
 };
