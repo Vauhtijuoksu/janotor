e895188c8cf45efb5317c8af9f466f41810ed325
Aleksi Seppänen <aleksi@seppanen.dev>
2021-11-06 16:57:13 +0200

    Use external_id when comparing donations

--- janotor/index.js
+++ janotor/index.js
@@ -4,33 +4,29 @@ module.exports = async function (context) {
     const apiResp = await axios.get(process.env.wwfUrl)
     const vauhtiApiResp = await axios.get(process.env.vauhtisUrl)
 
-    const latestDonation = vauhtiApiResp.data[vauhtiApiResp.data.length - 1];
-    const latestDonationTimestamp = new Date(latestDonation.timestamp);
-
-    for (const key in apiResp.data.entities) {
-        const element = apiResp.data.entities[key];
-        const timestamp = new Date(element.created_date);
-
-        if (timestamp < latestDonationTimestamp) {
+    const keys = Object.keys(apiResp.data.entities)
+    for await (const id of keys) {
+        if (vauhtiApiResp.data.some(e => e.external_id === id)) {
             continue
-        } else if (timestamp.getTime() === latestDonationTimestamp.getTime()) {
-            if (element.public_name === latestDonation.name && element.comment === latestDonation.message) {
-                continue
-            }
         }
+
+        const element = apiResp.data.entities[id];
+        const timestamp = new Date(element.created_date);
 
         const donation = {
             timestamp: timestamp.toISOString(),
-            name: element.public_name,
+            name: element.public_name || 'Anonyymi',
             message: element.comment || null,
-            amount: element.amount
+            amount: element.amount,
+            external_id: id
         }
 
-        await axios.post(process.env.vauhtisUrl, donation, {
+        axios.post(process.env.vauhtisUrl, donation, {
             auth: {
                 username: process.env.vauhtisUsername,
                 password: process.env.vauhtisPassword
             }
         });
+        context.log(`Added donation with id ${id}`)
     }
 };
