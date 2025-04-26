46485a9e8b931693e72dec49b67c5373506d50d4
Aleksi Seppänen <aleksi@seppanen.dev>
2025-04-26 17:06:02 +0300

    Add new implementation for mieli system

--- janotor/index.js
+++ janotor/index.js
@@ -1,26 +1,51 @@
 const axios = require('axios');
 
 module.exports = async function (context) {
-    const amount = Math.floor(Math.random() * (600 - 500) + 500);
-    const apiResp = await axios.get(`${process.env.siteUrl}/api/search/gift?page=1&per_page=${amount}&reference=${process.env.siteId}`)
+    const knownDonations = await handleDonationData(`${process.env.siteUrl}/api/${process.env.siteId}/donations`) 
+
+    if (knownDonations.length === 0) {
+        let page = 2
+        while (true) {
+            const knownDonations = await handleDonationData(`${process.env.siteUrl}/api/${process.env.siteId}/donations?page=${page}`)
+            if (knownDonations.length !== 0) {
+                break
+            }
+            page++
+        }
+    }
+};
+
+const handleDonationData = async (url) => {
+    const apiResp = await axios.get(url) 
     const vauhtiApiResp = await axios.get(process.env.vauhtisUrl)
 
     const promises = [];
-    const keys = Object.keys(apiResp.data.entities)
-    for await (const id of keys) {
-        if (vauhtiApiResp.data.some(e => e.external_id === id)) {
+    const donations = apiResp.data
+
+    if (vauhtiApiResp.data.length === apiResp.total) {
+        context.log('No new donations')
+        return
+    }
+
+    let knownDonations = 0
+
+    if (!apiResp.next_page_url) {
+        // Break the loop, last page
+        knownDonations = 1
+    }
+
+    for await (const d of donations) {
+        if (vauhtiApiResp.data.some(e => e.external_id === d.id)) {
+            knownDonations++
             continue
         }
 
-        const element = apiResp.data.entities[id];
-        const timestamp = new Date(element.created_date);
-
         const donation = {
-            timestamp: timestamp.toISOString(),
-            name: element.public_name || 'Anonyymi',
-            message: element.comment || null,
-            amount: element.amount,
-            external_id: id
+            timestamp: d.created_at.toISOString(),
+            name: d.name || 'Anonyymi',
+            message: d.message || null,
+            amount: d.amount,
+            external_id: d.id
         }
 
         const result = axios.post(process.env.vauhtisUrl, donation, {
@@ -36,4 +61,6 @@ module.exports = async function (context) {
     }
 
     await Promise.all(promises);
-};
+
+    return knownDonations
+}
